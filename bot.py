529696b440170b5c344601a1c469028e0600d8a8
democrazyfi <132473036+democrazyfi@users.noreply.github.com>
2025-01-27 00:09:55 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,10 +15,8 @@ class autocaption(Client):
     
     def __init__(self):
         super().__init__(
-            session_name="Captioner",
+            "Captioner",
             bot_token = Config.BOT_TOKEN,
-            api_id = Config.API_ID,
-            api_hash = Config.API_HASH,
             workers = 20,
             plugins = dict(
                 root="Plugins"
